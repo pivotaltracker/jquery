9751f0473bb3a8c950656fe7d4f5c7fb8579b495
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2019-03-25 17:57:30 +0100

    Core: Prevent Object.prototype pollution for $.extend( true, ... )
    Closes gh-4333
    
    Signed-off-by: Benjamin Cutler <bcutler@pivotal.io>

--- dist/jquery.js
+++ dist/jquery.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://jquery.org/license
  *
- * Date: 2016-05-20T17:23Z
+ * Date: 2019-07-26T16:24Z
  */
 
 (function( global, factory ) {
@@ -209,8 +209,9 @@ jQuery.extend = jQuery.fn.extend = function() {
 				src = target[ name ];
 				copy = options[ name ];
 
+				// Prevent Object.prototype pollution
 				// Prevent never-ending loop
-				if ( target === copy ) {
+				if ( name === "__proto__" || target === copy ) {
 					continue;
 				}
 
